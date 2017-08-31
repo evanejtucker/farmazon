cb1e99bd2803b543df1dd555cb63d0ad02e7ea61
nateodes <nateodes@gmail.com>
2017-08-31 12:42:52 -0600

    added jokes span

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,4 +33,10 @@ export class AppComponent {
     this.state = (this.state === 'small' ? 'large' : 'small')
   }
 
+jokeBank: any = ["Why did the cabbage win the race?.....because it was ahead", "What is a farmers favorite Bruce Springsteen song?...Born in the USDA ", "Where do farmers send their kids?....Kinder-Garden", "What do you call a Nebraskan farmer with a sheep under each arm?....A pimp", "What's the best part of farming?....Getting down and dirty with my hoes", "What do you get when you cross an elephant with a garden?.....Squash!", "How did the chicken farmer get into the Guinness book of world records?...His record c--k"]
+
+  // create random word, use example from rps 
+randomJoke: string = this.jokeBank[Math.floor(Math.random() * this.jokeBank.length)];
+
+
 }
